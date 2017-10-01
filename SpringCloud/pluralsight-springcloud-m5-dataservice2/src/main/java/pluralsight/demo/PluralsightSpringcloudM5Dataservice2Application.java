35facc65e828a5bd2e4a14dc8599a585283f10ae
Rakesh Nair <nair.fm@gmail.com>
2017-10-01 19:20:00 -0400

    working

--- SpringCloud/pluralsight-springcloud-m5-dataservice2/src/main/java/pluralsight/demo/PluralsightSpringcloudM5Dataservice2Application.java
+++ SpringCloud/pluralsight-springcloud-m5-dataservice2/src/main/java/pluralsight/demo/PluralsightSpringcloudM5Dataservice2Application.java
@@ -3,8 +3,11 @@ package pluralsight.demo;
 import java.util.Hashtable;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,19 +18,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PluralsightSpringcloudM5Dataservice2Application {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PluralsightSpringcloudM5Dataservice2Application.class, args);
 	}
 	
+	@Autowired
+	private Tracer tracer;
+	
+	
+	
+	
+	
 	@RequestMapping(value="/customer/{cid}/vehicledetails", method=RequestMethod.GET)
-	public @ResponseBody String getCustomerVehicleDetails(@PathVariable Integer cid) {
-		
-		Hashtable<Integer, String> vehicles = new Hashtable<Integer, String>();
-		vehicles.put(100, "Lincoln Continental; Plate SNUG30");
-		vehicles.put(101, "Chevrolet Camaro; Plate R7TYR43");
-		vehicles.put(102, "Volkswagen Beetle; Plate 6CVI3E2");
-		
-		String result = vehicles.get(cid);
+	public @ResponseBody String getCustomerVehicleDetails(@PathVariable Integer cid) throws InterruptedException {
+		//pretend we have db call and will create a new span
+		Span span = this.tracer.createSpan("lookupvehicle");
+	
+					
+		String result;
+		try {
+			this.tracer.addTag("customerId", cid.toString());
+			span.logEvent("---DB Event started");
+			Thread.sleep(500);
+			Hashtable<Integer, String> vehicles = new Hashtable<Integer, String>();
+			vehicles.put(100, "Lincoln Continental; Plate SNUG30");
+			vehicles.put(101, "Chevrolet Camaro; Plate R7TYR43");
+			vehicles.put(102, "Volkswagen Beetle; Plate 6CVI3E2");
+			
+			result = vehicles.get(cid);
+			
+			span.logEvent("---DB Event finished");
+		}  finally {
+			this.tracer.close(span);
+		}
 		
 		return result;
 	}
